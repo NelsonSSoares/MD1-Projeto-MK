034a44ac4dfab3dd2622f0f0bf2fc4984cfee91e
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-08-11 15:49:13 -0300

    corrigindobug

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-/*
-function musica(){
-    const musica = new Audio('../audios/choose.mp3')
+
+/*function musica(){
+    const musica = new Audio('audios/choose.mp3').
     musica.play();
     musica.loop = true;
 }
 */
 function scorpion(){
-    document.getElementById('escolhido').src = '../img/personagens/scorpion/escolhido1.gif';
+    document.getElementById('escolhido').src = 'img/personagens/scorpion/escolhido1.gif';
     document.getElementById('escolhido').style.transform = 'scale(1.3)';
 
 }
 
 function subzero(){
-    document.getElementById('escolhido2').src = '../img/personagens/subzero/escolhido2.gif';
+    document.getElementById('escolhido2').src = 'img/personagens/subzero/escolhido2.gif';
     document.getElementById('escolhido2').style.transform = 'scale(1.3)';
 }
 
 function ermac(){
-    document.getElementById('escolhido3').src = '../img/personagens/ermac/escolhido3.gif';
+    document.getElementById('escolhido3').src = 'img/personagens/ermac/escolhido3.gif';
     document.getElementById('escolhido3').style.transform = 'scale(1.3)';
 }
