a0933a80c4eb711321d6d1b24ce9e52b5add4a38
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-08-12 12:05:47 -0300

    chooseyourfighter

--- js/index.js
+++ js/index.js
@@ -9,20 +9,20 @@ function scorpion(){
     document.getElementById('escolhido').src = 'img/personagens/scorpion/escolhido1.gif';
     document.getElementById('escolhido').style.transform = 'scale(1.4)';
     let scorpion = document.getElementById('escolhido');
-    scorpion.style.paddingBottom = '7px';
+    scorpion.style.paddingBottom = '13px';
 }
 
 function subzero(){
     document.getElementById('escolhido2').src = 'img/personagens/subzero/escolhido2.gif';
-    document.getElementById('escolhido2').style.transform = 'scale(1.5)';
     let subzero = document.getElementById('escolhido2');
-    subzero.style.paddingBottom = '18px';
+    subzero.style.paddingBottom = '30px';
+    document.getElementById('escolhido2').style.transform = 'scale(1.5)';
 }
 
 function ermac(){
     document.getElementById('escolhido3').src = 'img/personagens/ermac/escolhido3.gif';
-    document.getElementById('escolhido3').style.transform = 'scale(1.5)';
     let ermac = document.getElementById('escolhido3');
-    ermac.style.paddingLeft = '15px';
-    ermac.style.paddingBottom = '23px';
+    ermac.style.paddingLeft = '18px';
+    ermac.style.paddingBottom = '40px';
+    document.getElementById('escolhido3').style.transform = 'scale(1.5)';
 }
