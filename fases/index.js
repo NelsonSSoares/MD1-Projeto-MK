e0572b8b32cd7d1602b8397dd7f3f97db128c034
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-08-16 20:21:10 -0300

    torres personagens

--- fases/index.js
+++ fases/index.js
@@ -1,8 +1,19 @@
 function musica(){
     const musica = new Audio('../audios/choose.mp3');
     musica.play();
+
+    setTimeout(() => {
+        window.location = 'stage1/index.html'
+    }, 4000);
+
+    const haha = new Audio('../audios/system/haha2.mp3');
+    setTimeout(() => {
+        haha.play() 
+    }, 1000);
+   
 }
 function historia(){
     const historia = new Audio('../audios/historia.mp3');
     historia.play();
 }
+
