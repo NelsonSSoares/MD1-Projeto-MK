1cf90e8b85fee58b7088e0149a9b6b134c756ae9
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-08-16 22:51:11 -0300

    GOLPES SCORPION

---
+++ js/golpes_scorpion.js
@@ -0,0 +1,42 @@
+function soco(){
+    document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/soco.gif';
+    let scorpion = document.getElementById('img-scorpion');
+    scorpion.style.transform = 'scale(1.5)'
+    setTimeout(() => {
+        scorpion.style.transform = 'scale(1)'
+        return document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/fighter.gif';
+    }, 1000);
+}
+
+function machadada(){
+    document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/machadada.gif';
+    let scorpion = document.getElementById('img-scorpion');
+    scorpion.style.transform = 'scale(1.8)'
+    setTimeout(() => {
+        scorpion.style.transform = 'scale(1)'
+        return document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/fighter.gif';
+    }, 1000);
+}
+
+function joelhada(){
+    document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/joelhada.gif';
+    let scorpion = document.getElementById('img-scorpion');
+    scorpion.style.transform = 'scale(1.5)'
+    setTimeout(() => {
+        scorpion.style.transform = 'scale(1)'
+        return document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/fighter.gif';
+    }, 600);
+}
+
+function comeHere(){
+    const comehere = new Audio('../../audios/scorpion/golpes/comehere.mp3');
+    comehere.play();
+    document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/comehere.gif';
+    let scorpion = document.getElementById('img-scorpion');
+    scorpion.style.transform = 'scale(1.3)'
+    setTimeout(() => {
+        scorpion.style.transform = 'scale(1)'
+        return document.getElementById('img-scorpion').src = '../../img/personagens/scorpion/fighter.gif';
+    }, 1800);
+}
+
